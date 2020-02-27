22a107f2cfdce75fdc13bac46cda9c12ad18e635
Dirk Hartmann <2monex@gmx.net>
2020-02-27 20:14:21 +0100

    Added two more string conversion test cases

--- test/conversion_tests.h
+++ test/conversion_tests.h
@@ -33,5 +33,19 @@ TEST_F( StringConversionTest, test_string_conversion_round_half_even )
   ASSERT_THAT( fourDigitDecimalValue.toString( 2 ), Eq( "10.00" ) );
 }
 
+TEST_F( StringConversionTest, test_string_conversion_round_half_down )
+{
+  ASSERT_THAT( twoDigitDecimalValue.toString( 1, mpdecimal::RoundMode::RoundDown ), Eq( "5.5" ) );
+  ASSERT_THAT( threeDigitDecimalValue.toString( 2, mpdecimal::RoundMode::RoundDown ), Eq( "-5.00" ) );
+  ASSERT_THAT( fourDigitDecimalValue.toString( 2, mpdecimal::RoundMode::RoundDown ), Eq( "10.00" ) );
+}
+
+TEST_F( StringConversionTest, test_string_conversion_round_trunc )
+{
+  ASSERT_THAT( twoDigitDecimalValue.toString( 1, mpdecimal::RoundMode::RoundTrunc ), Eq( "5.5" ) );
+  ASSERT_THAT( threeDigitDecimalValue.toString( 2, mpdecimal::RoundMode::RoundTrunc ), Eq( "-5.00" ) );
+  ASSERT_THAT( fourDigitDecimalValue.toString( 2, mpdecimal::RoundMode::RoundTrunc ), Eq( "10.00" ) );
+}
+
 #endif // H_A7044423D0E2461989AA77A609704686
 
