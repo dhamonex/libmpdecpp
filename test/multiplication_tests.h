6635a194d0f69883b08deb052047d11482d93c97
Dirk Hartmann <2monex@gmx.net>
2020-03-09 20:53:36 +0100

    Added andditinal test

--- test/multiplication_tests.h
+++ test/multiplication_tests.h
@@ -34,4 +34,13 @@ TEST_F( MultiplicationTest, multiply_and_copy_test )
   ASSERT_THAT( b, Eq( 6 ) );
 }
 
+TEST_F( MultiplicationTest, multiply_assoziativ )
+{
+  auto resultA = a * (b * c);
+  auto resultB = ( a * b ) * c;
+  
+  ASSERT_THAT( resultA, Eq( resultB ) );
+  ASSERT_THAT( resultA.toInt64(), Eq( -30 ) );
+}
+
 #endif // H_CB603DE2F3D24B2D8929ED8A7F332398
